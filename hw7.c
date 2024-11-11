75b2da36e3bb4b27983f82b57c54cb3eb7f01919
thdgus0307 <bsh0374@gmail.com>
2024-11-11 14:29:52 +0900

    hw7

---
+++ hw7.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main() {
+
+    int arr1[6] = { 1, 2, 3, 4, 5, 6 };
+    int arr2[6] = { 7, 8, 9, 10, 11, 12 };
+
+
+    int* ptr1 = arr1;
+    int* ptr2 = arr2;
+
+    printf("arr1: ");
+    for (int i = 0; i < 6; i++) {
+        printf("%d ", arr1[i]);
+    }
+    printf("\n");
+
+    printf("arr2: ");
+    for (int i = 0; i < 6; i++) {
+        printf("%d ", arr2[i]);
+    }
+    printf("\n");
+    printf("\n");
+
+
+    for (int i = 0; i < 6; i++) {
+        int temp = *(ptr1 + i);
+        *(ptr1 + i) = *(ptr2 + i);
+        *(ptr2 + i) = temp;
+    }
+
+    
+    printf("after swap\n");
+    printf("arr1: ");
+    for (int i = 0; i < 6; i++) {
+        printf("%d ", arr1[i]);
+    }
+    printf("\n");
+
+    printf("arr2: ");
+    for (int i = 0; i < 6; i++) {
+        printf("%d ", arr2[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
